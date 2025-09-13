613644334020ff257ba57867acb8403437e4f40b
Clay Speakman <clay@healthrocket.life>
2025-09-13 19:45:48 -0400

    Fix useCallback import error

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import { MessageBubble } from './MessageBubble';
 import { LoadingIndicator } from './LoadingIndicator';
 import { ChatInput } from './ChatInput';
